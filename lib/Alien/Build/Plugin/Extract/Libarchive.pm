c8b0f74f8d71bf87ead5ee96155ba1c257d5bd40
Graham Ollis <plicease@cpan.org>
2021-06-23 15:20:39 -0600

    Initial structure

---
+++ lib/Alien/Build/Plugin/Extract/Libarchive.pm
@@ -0,0 +1,12 @@
+package Alien::Build::Plugin::Extract::Libarchive;
+
+use strict;
+use warnings;
+use 5.008004;
+
+# ABSTRACT: Alien::Build plugin to extract a tarball using libarchive
+# VERSION
+
+1;
+
+
